388f3d2c5125dc17083c157a0db8f997155c7efd
qiangchunyu <1715464388@qq.com>
2022-05-07 00:18:42 +0800

    --other 两数之和

---
+++ hot100/easy/twonumsum.go
@@ -0,0 +1,34 @@
+package easy
+
+// TwoSum1 暴力版
+func TwoSum1(nums []int, target int) []int {
+	if nums == nil {
+		return nil
+	}
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return nil
+}
+
+// TwoSum 答案版
+func TwoSum(nums []int, target int) []int {
+	if nums == nil {
+		return nil
+	}
+
+	set := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if val, ok := set[nums[i]]; ok {
+			return []int{val, i}
+		}
+		set[target-nums[i]] = i
+	}
+
+	return nil
+}
