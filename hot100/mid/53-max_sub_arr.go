b35842b36a238616a5d41e6c81a4bfee250fb874
qiangchunyu <1715464388@qq.com>
2022-10-06 10:49:47 +0800

    --other 10.6

---
+++ hot100/mid/53-max_sub_arr.go
@@ -0,0 +1,44 @@
+package mid
+
+import "math"
+
+/*
+53. 最大子数组和
+给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+子数组 是数组中的一个连续部分。
+
+示例 1：
+
+输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出：6
+解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+示例 2：
+
+输入：nums = [1]
+输出：1
+示例 3：
+
+输入：nums = [5,4,-1,7,8]
+输出：23
+*/
+func maxSubArray(nums []int) int {
+	length := len(nums)
+	if length == 0 {
+		return 0
+	}
+	if length == 1 {
+		return nums[0]
+	}
+	res, sum := math.MinInt64, 0
+	for _, num := range nums {
+		sum += num
+		if sum > res {
+			res = sum
+		}
+		if sum <= 0 {
+			sum = 0
+		}
+	}
+
+	return res
+}
